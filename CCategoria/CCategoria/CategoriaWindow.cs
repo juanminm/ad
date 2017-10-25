71b1cc100fbd52a01276ec1a6ed5f440cba2cbea
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2017-10-25 10:44:25 +0200

    Refactor and reformat code

--- CCategoria/CCategoria/CategoriaWindow.cs
+++ CCategoria/CCategoria/CategoriaWindow.cs
@@ -8,8 +8,14 @@ namespace CCategoria
     public partial class CategoriaWindow : Gtk.Window
     {
         object id;
-        public CategoriaWindow(object id) : this (){
+        public CategoriaWindow(object id) :
+                base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
+
+            Title = "Categoria";
             this.id = id;
+
             IDbCommand dbCommand = App.Instance.Connection.CreateCommand();
             dbCommand.CommandText = "SELECT * FROM `categoria` " +
                 "WHERE `id` = @id";
@@ -19,44 +25,49 @@ namespace CCategoria
             string nombre = (string)dataReader["nombre"];
             dataReader.Close();
             entryNombre.Text = nombre;
+
+            saveAction.Activated += delegate
+            {
+                update();
+                Destroy();
+            };
+
         }
 
         public CategoriaWindow() :
-				base(Gtk.WindowType.Toplevel)
-		{
-			this.Build();
+                base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
 
-			Title = "Categoria";
+            Title = "Categoria";
 
-			saveAction.Activated += delegate
-			{
-                if (id == null)
-                    insert();
-                else
-                    update();
-
-				Destroy();
-			};
-		}
-
-        private void insert() {
-			IDbCommand dbCommand = App.Instance.Connection.CreateCommand();
-
-			dbCommand.CommandText = "INSERT INTO `categoria` (`nombre`) "
-				+ "VALUES (@nombre)";
-			DbCommandHelper.AddParameter(dbCommand, "nombre",
-										 entryNombre.Text);
-
-			dbCommand.ExecuteNonQuery();
+            saveAction.Activated += delegate
+            {
+                insert();
+                Destroy();
+            };
         }
 
-        private void update() {
-			IDbCommand dbCommand = App.Instance.Connection.CreateCommand();
-			dbCommand.CommandText = "UPDATE `categoria` SET `nombre` = " +
+        private void insert()
+        {
+            IDbCommand dbCommand = App.Instance.Connection.CreateCommand();
+
+            dbCommand.CommandText = "INSERT INTO `categoria` (`nombre`) "
+                + "VALUES (@nombre)";
+            DbCommandHelper.AddParameter(dbCommand, "nombre",
+                                         entryNombre.Text);
+
+            dbCommand.ExecuteNonQuery();
+        }
+
+        private void update()
+        {
+            IDbCommand dbCommand = App.Instance.Connection.CreateCommand();
+            dbCommand.CommandText = "UPDATE `categoria` SET `nombre` = " +
                 "@nombre WHERE `id` = @id";
-			DbCommandHelper.AddParameter(dbCommand, "id", id);
-			DbCommandHelper.AddParameter(dbCommand, "nombre", entryNombre.Text);
-			dbCommand.ExecuteNonQuery();
+            DbCommandHelper.AddParameter(dbCommand, "id", id);
+            DbCommandHelper.AddParameter(dbCommand, "nombre", entryNombre.Text);
+            dbCommand.ExecuteNonQuery();
         }
-	}
+    }
 }
