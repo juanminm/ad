abc220c2ff68eef4ae0c4b3b135ff5e65d2da332
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2017-11-08 12:12:48 +0100

    Add ArticuloWindow and open it with MainWindow

--- CArticulo/CArticulo/MainWindow.cs
+++ CArticulo/CArticulo/MainWindow.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using Gtk;
 
+using CArticulo;
+
 public partial class MainWindow : Gtk.Window
 {
     public MainWindow() : base(Gtk.WindowType.Toplevel)
     {
         Build();
+
+        newAction.Activated += delegate {
+            new ArticuloWindow();
+        };
     }
 
     protected void OnDeleteEvent(object sender, DeleteEventArgs a)
