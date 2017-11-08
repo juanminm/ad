b2e5e2c8499175e266ecddfb5825c17f37f95d06
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2017-11-08 11:26:26 +0100

    Create Articulo class

---
+++ CArticulo/CArticulo/Articulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System;
+namespace CArticulo
+{
+    public class Articulo
+    {
+        public Articulo()
+        {
+            nombre = "";
+        }
+
+        private long id;
+        private string nombre;
+        private long categoriaId;
+
+        public long Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public long CategoriaId
+        {
+            get { return categoriaId; }
+            set { categoriaId = value; }
+        }
+    }
+}
