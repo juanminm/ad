ab33a1381b11cc588ef213d11774e2593c6690ae
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2017-11-22 12:56:05 +0100

    Add dynamic multi-column ComboBox creation

--- CSerpisAd/CSerpisAd/ComboBoxHelper.cs
+++ CSerpisAd/CSerpisAd/ComboBoxHelper.cs
@@ -6,55 +6,74 @@ namespace Serpis.Ad
 {
     public class ComboBoxHelper
     {
-        public static void Init(ComboBox comboBox)
+        private static void Init(ComboBox comboBox, IDataReader dataReader)
         {
             if (comboBox.Model != null)
                 return;
 
-            comboBox.AddAttribute(new CellRendererText(), "text", 0);
+            Type[] types = new Type[dataReader.FieldCount];
 
-            ListStore listStore = new ListStore(typeof(string));
+            for (int i = 0; i < dataReader.FieldCount; i++)
+            {
+                types[i] = typeof(string);
+            }
+
+            CellRendererText nombreCellRendererText = new CellRendererText();
+            comboBox.PackStart(nombreCellRendererText, false);
+            comboBox.AddAttribute(nombreCellRendererText, "text", 1);
+
+            ListStore listStore = new ListStore(types);
             comboBox.Model = listStore;
         }
 
-        private static void FillListStore(ListStore listStore,
-                                        IDataReader dataReader)
+        private static void FillListStore(ComboBox comboBox,
+                                          IDataReader dataReader, object id,
+                                          Boolean withNull)
         {
+            ListStore listStore = (ListStore)comboBox.Model;
+            TreeIter treeIter = new TreeIter();
+
             while (dataReader.Read())
             {
                 object[] row = new object[dataReader.FieldCount];
 
                 for (int i = 0; i < row.Length; i++)
+                {
                     row[i] = dataReader.GetValue(i).ToString();
+
+                    if (!withNull && i == 0 && id.Equals(dataReader[0]))
+                        listStore.IterNthChild(out treeIter, i);
+                }
 
                 listStore.AppendValues(row);
             }
+
+
+            if (withNull)
+            {
+                listStore.Insert(0);
+                listStore.GetIterFirst(out treeIter);
+                comboBox.SetActiveIter(treeIter);
+            }
+            else
+            {
+                comboBox.SetActiveIter(treeIter);
+            }
         }
 
-        public static void Fill(ComboBox comboBox, String sqlString)
+        public static void Fill(ComboBox comboBox, String sqlString, object id,
+                               Boolean withNull)
         {
             IDataReader dataReader;
             IDbCommand dbCommand = App.Instance.Connection.CreateCommand();
-            ListStore listStore;
 
             dbCommand.CommandText = sqlString;
             dataReader = dbCommand.ExecuteReader();
 
-            Init(comboBox);
-            listStore = (ListStore)comboBox.Model;
-            FillListStore(listStore, dataReader);
+            Init(comboBox, dataReader);
+            FillListStore(comboBox, dataReader, id, withNull);
 
             dataReader.Close();
-        }
-
-
-        public static void Fill(ComboBox comboBox, string selectSql,
-                                bool withNull)
-        {
-            Fill(comboBox, selectSql);
-
-            if (withNull)
-                ((ListStore)comboBox.Model).Insert(0);
         }
     }
 }
