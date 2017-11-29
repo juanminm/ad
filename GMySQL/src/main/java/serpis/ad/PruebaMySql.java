8f029157fcffe827aa105dda51aaa6f5c0353f7a
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2017-11-29 10:19:56 +0100

    Create menu

--- GMySQL/src/main/java/serpis/ad/PruebaMySql.java
+++ GMySQL/src/main/java/serpis/ad/PruebaMySql.java
@@ -2,47 +2,60 @@ package serpis.ad;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Scanner;
 
 public class PruebaMySql {
 
 	public static void main(String[] args) throws SQLException {
 		Connection connection = DriverManager.getConnection(
-				"jdbc:mysql://localhost/dbprueba?useSSL=false", "root", "sistemas");
+				"jdbc:mysql://localhost/dbprueba?useSSL=false", "root",
+				"sistemas");
+		boolean isExit = false;
 		
-		try (
-				Statement stmt = connection.createStatement();
-				ResultSet rs = stmt.executeQuery("SELECT * FROM `categoria`"
-						+ "ORDER BY `id`")
-			) {
-			while (rs.next()) {
-				int c = rs.getMetaData().getColumnCount();
-				
-				for (int i = 1; i <= c; i++) {
-					System.out.print(rs.getString(i));
-					
-					if (i != c)
-						System.out.print(", ");
-				}				
-				
-				System.out.println();
+		while(!isExit) {
+			Scanner scan = new Scanner(System.in);
+			int option;
+
+			System.out.println("0. Salir");
+			System.out.println("1. Nuevo");
+			System.out.println("2. Modificar");
+			System.out.println("3. Eliminar");
+			System.out.println("4. Consultar");
+			System.out.println("5. Listar");
+			System.out.println();
+			System.out.print("Escoge una opción: ");
+			
+			option = scan.nextInt();
+			scan.nextLine();
+			
+			switch (option) {
+				case 0:
+					isExit = true; 
+					break;
+				case 1:
+					//TODO Nuevo
+					break;
+				case 2:
+					//TODO Modificar
+					break;
+				case 3:
+					//TODO Eliminar
+					break;
+				case 4:
+					//TODO Consultar
+					break;
+				case 5:
+					//TODO Listar
+					break;
+				default:
+					break;
 			}
+			
+			scan.close();
 		}
 		
 		connection.close();
-		
-		/*
-		 * TODO
-		 * 0. Salir.
-		 * 1. Nuevo.
-		 * 2. Modificar.
-		 * 3. Eliminar.
-		 * 4. Consultar.
-		 * 5. Listar
-		 * 
-		 */
 	}
 
 }
