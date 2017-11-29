3c83b3896658381e00595ab6e2974656faf5d7e6
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2017-11-29 12:28:59 +0100

    Rename to Main

--- GMySQL/src/main/java/serpis/ad/Main.java
+++ GMySQL/src/main/java/serpis/ad/Main.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
-public class PruebaMySql {
+public class Main {
 
 	public static void main(String[] args) throws SQLException {
 		Connection connection = DriverManager.getConnection(
