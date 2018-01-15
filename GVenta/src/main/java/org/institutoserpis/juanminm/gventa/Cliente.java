f391dcc82315783d3d9d57f60de17b862a9b28be
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2018-01-15 13:44:53 +0100

    Add Cliente class

---
+++ GVenta/src/main/java/org/institutoserpis/juanminm/gventa/Cliente.java
@@ -0,0 +1,37 @@
+package org.institutoserpis.juanminm.gventa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity(name = "cliente")
+public class Cliente {
+	@Id
+	@GeneratedValue
+	private long id;
+
+	@Column(name = "`nombre`")
+	private String nombre;
+
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+
+	public Cliente() {}
+
+	public Cliente(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+}
