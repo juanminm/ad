7629ea7baa27ad20797e10590780450f99b72e6a
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2018-01-15 13:44:40 +0100

    Add Articulo class

---
+++ GVenta/src/main/java/org/institutoserpis/juanminm/gventa/Articulo.java
@@ -0,0 +1,56 @@
+package org.institutoserpis.juanminm.gventa;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+//@Entity(name = "articulo")
+@Entity
+public class Articulo {
+	@Id
+	@GeneratedValue
+	private long id;
+
+	@Column(name = "`nombre`")
+	private String nombre;
+
+	@Column(name = "`precio`")
+	private BigDecimal precio;
+
+	@ManyToOne
+	@JoinColumn(name = "categoria",
+	foreignKey = @ForeignKey(name = "articulo_ibfk_1")
+	)
+	private Categoria categoria;
+
+	public Articulo() {
+	}
+
+	public Articulo(String nombre, BigDecimal precio, Categoria categoria) {
+		this.nombre = nombre;
+		this.precio = precio;
+		this.categoria = categoria;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public BigDecimal getPrecio() {
+		return precio;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+}
