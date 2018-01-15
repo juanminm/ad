f1889f26fc006a201a62f2d3576519ffc9236980
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2018-01-15 13:51:08 +0100

    Add VentaMain class

---
+++ GVenta/src/main/java/org/institutoserpis/juanminm/gventa/VentaMain.java
@@ -0,0 +1,32 @@
+package org.institutoserpis.juanminm.gventa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class VentaMain {
+
+	private static EntityManagerFactory entityManagerFactory;
+
+	public static void main(String[] args) {
+		entityManagerFactory =
+				Persistence.createEntityManagerFactory(
+						"org.institutoserpis.juanminm.gventa");
+		showAll();
+		entityManagerFactory.close();
+	}
+
+	private static void showAll() {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		List<Categoria> categorias = entityManager
+				.createQuery("from Categoria order by id", Categoria.class)
+				.getResultList();
+		for (Categoria categoria : categorias)
+			System.out.println(categoria);
+		entityManager.getTransaction().commit();
+	}
+
+}
