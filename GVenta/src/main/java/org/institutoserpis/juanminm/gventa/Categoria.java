d33e552df5543d60d26916ee8707cd6ce4ddb674
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2018-01-15 13:44:12 +0100

    Add Categoria class

---
+++ GVenta/src/main/java/org/institutoserpis/juanminm/gventa/Categoria.java
@@ -0,0 +1,44 @@
+package org.institutoserpis.juanminm.gventa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+//@Entity(name = "categoria")
+@Entity
+public class Categoria {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name = "`nombre`")
+	private String nombre;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Articulo> articulos = new ArrayList<>();
+
+	public Categoria() {}
+
+	public Categoria(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("[%s] %s", id, nombre);
+	}
+}
